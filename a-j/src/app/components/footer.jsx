9106f744fe40953bcf37176e7c051449e50f33a4
harfore <remiharfoush@gmail.com>
2025-03-05 14:51:17 +0100

    add: linkedin and mail contact links

--- a-j/src/app/components/footer.jsx
+++ a-j/src/app/components/footer.jsx
@@ -3,7 +3,8 @@ export default function Footer() {
         <div className="h-1/9">
             <ol>
                 <a href="https://www.instagram.com/amjadjanjoun/"><li>Instagram</li></a>
-                <li>Mail</li>
+                <a href="https://www.linkedin.com/in/amjad-janjoun-6151a128a/"><li>Linkedin</li></a>
+                <a href="mailto:amjadjanjoun5@gmail.com"></a><li>Mail</li>
             </ol>
         </div>
     )
